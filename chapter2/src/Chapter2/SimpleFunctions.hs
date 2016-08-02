4f6d41ff249019d55d92b7a058eee7cf9c7661f0
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2016-08-01 22:28:21 -0700

    Add +++ binary operator for concatenation

--- chapter2/src/Chapter2/SimpleFunctions.hs
+++ chapter2/src/Chapter2/SimpleFunctions.hs
@@ -1,2 +1,6 @@
 firstOrEmpty :: [[Char]] -> [Char]
 firstOrEmpty lst = if not (null lst) then head lst else "empty"
+
+lst1 +++ lst2 = if null lst1
+    then lst2
+    else (head lst1) : (tail lst1 +++ lst2)
