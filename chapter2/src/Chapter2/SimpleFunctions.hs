340f3c75d05318e8b16cf62d39c05bb5055cb0f1
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2016-08-01 22:30:00 -0700

    Add reverse2 function to reverse lists

--- chapter2/src/Chapter2/SimpleFunctions.hs
+++ chapter2/src/Chapter2/SimpleFunctions.hs
@@ -4,3 +4,7 @@ firstOrEmpty lst = if not (null lst) then head lst else "empty"
 lst1 +++ lst2 = if null lst1
     then lst2
     else (head lst1) : (tail lst1 +++ lst2)
+
+reverse2 list = if null list
+    then []
+    else reverse2 (tail list) +++ [head list]
