4481c946eec1ee3f740255aea72b10701cebce67
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2016-08-02 06:53:50 -0700

    Add function type signatures

--- chapter2/src/Chapter2/SimpleFunctions.hs
+++ chapter2/src/Chapter2/SimpleFunctions.hs
@@ -3,14 +3,17 @@ module Chapter2.SimpleFunctions where
 firstOrEmpty :: [[Char]] -> [Char]
 firstOrEmpty lst = if not (null lst) then head lst else "empty"
 
+(+++) :: [a] -> [a] -> [a]
 lst1 +++ lst2 = if null lst1
     then lst2
     else (head lst1) : (tail lst1 +++ lst2)
 
+reverse2 :: [a] -> [a]
 reverse2 list = if null list
     then []
     else reverse2 (tail list) +++ [head list]
 
+maxmin :: Ord a => [a] -> (a, a)
 maxmin list = let h = head list
     in if null (tail list)
         then (h, h)
