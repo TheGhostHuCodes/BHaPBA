f27e88066c2504492210dae7065ad52864f35ed2
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2016-08-01 22:30:34 -0700

    Add function to return max and min of list as tuple

--- chapter2/src/Chapter2/SimpleFunctions.hs
+++ chapter2/src/Chapter2/SimpleFunctions.hs
@@ -8,3 +8,12 @@ lst1 +++ lst2 = if null lst1
 reverse2 list = if null list
     then []
     else reverse2 (tail list) +++ [head list]
+
+maxmin list = let h = head list
+    in if null (tail list)
+        then (h, h)
+        else (if h > t_max then h else t_max
+            , if h < t_min then h else t_min)
+            where t = maxmin (tail list)
+                  t_max = fst t
+                  t_min = snd t
