2ddb0860c61a2a0b8e07579ef7c509d00b411b61
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2016-08-01 23:03:20 -0700

    Add Client, Person, and Gender data types

--- chapter2/src/Chapter2/SimpleFunctions.hs
+++ chapter2/src/Chapter2/SimpleFunctions.hs
@@ -17,3 +17,14 @@ maxmin list = let h = head list
             where t = maxmin (tail list)
                   t_max = fst t
                   t_min = snd t
+
+data Client = GovOrg        String
+            | Company       String Integer Person String
+            | Individual    Person Bool
+            deriving Show
+
+data Person = Person String String Gender
+            deriving Show
+
+data Gender = Male | Female | Unknown
+            deriving Show
