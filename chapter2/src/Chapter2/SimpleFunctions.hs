6f3ae6866d9333c854de3df847684d4d215d08c8
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2016-08-03 17:27:15 -0700

    Refactor maxmin function using pattern matching

--- chapter2/src/Chapter2/SimpleFunctions.hs
+++ chapter2/src/Chapter2/SimpleFunctions.hs
@@ -13,14 +13,10 @@ reverse2 list = if null list
     else reverse2 (tail list) +++ [head list]
 
 maxmin :: Ord a => [a] -> (a, a)
-maxmin list = let h = head list
-    in if null (tail list)
-        then (h, h)
-        else (if h > t_max then h else t_max
-            , if h < t_min then h else t_min)
-            where t = maxmin (tail list)
-                  t_max = fst t
-                  t_min = snd t
+maxmin [x] = (x, x)
+maxmin (x:xs) = (if x > xs_max then x else xs_max
+                , if x < xs_min then x else xs_min)
+                where (xs_max, xs_min) = maxmin xs
 
 data Client = GovOrg        String
             | Company       String Integer Person String
