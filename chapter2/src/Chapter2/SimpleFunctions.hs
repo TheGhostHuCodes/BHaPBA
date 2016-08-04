b9346ed1c1a9c40f7c4550d1f3f38d3815155c2b
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2016-08-03 17:22:20 -0700

    Refactor +++ operator using pattern matching

--- chapter2/src/Chapter2/SimpleFunctions.hs
+++ chapter2/src/Chapter2/SimpleFunctions.hs
@@ -4,9 +4,8 @@ firstOrEmpty :: [[Char]] -> [Char]
 firstOrEmpty lst = if not (null lst) then head lst else "empty"
 
 (+++) :: [a] -> [a] -> [a]
-lst1 +++ lst2 = if null lst1
-    then lst2
-    else (head lst1) : (tail lst1 +++ lst2)
+[]     +++ list2 = list2
+(x:xs) +++ list2 = x:(xs +++ list2)
 
 reverse2 :: [a] -> [a]
 reverse2 list = if null list
