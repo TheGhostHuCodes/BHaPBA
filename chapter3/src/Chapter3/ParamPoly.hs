76802f5051c47de61749f2f58e8e68e9f4fd1c98
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2016-08-04 21:51:54 -0700

    Add data type with polymorphic type

--- chapter3/src/Chapter3/ParamPoly.hs
+++ chapter3/src/Chapter3/ParamPoly.hs
@@ -3,3 +3,14 @@ module Chapter3.ParamPoly where
 maybeString :: Maybe t -> [Char]
 maybeString (Just _) = "Just"
 maybeString Nothing  = "Nothing"
+
+data Client i = GovOrg      { clientId :: i, clientName :: String }
+              | Company     { clientId :: i
+                             , clientName :: String
+                             , person :: Person
+                             , duty :: String }
+              | Individual  { clientId :: i, person :: Person }
+              deriving Show
+
+data Person = Person { firstName :: String, lastName :: String }
+            deriving Show
