64667babe6523f81d668977366d5212b6ec33044
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2016-08-06 12:01:38 -0700

    Add function to check if tuple elements are equal

--- chapter3/src/Chapter3/FnsParams.hs
+++ chapter3/src/Chapter3/FnsParams.hs
@@ -2,3 +2,6 @@ module Chapter3.FnsParams where
 
 apply3f2 :: (Integer -> Integer) -> Integer -> Integer
 apply3f2 f x = 3 * f(x + 2)
+
+equalTuples :: [(Integer, Integer)] -> [Bool]
+equalTuples t = map (\(x, y) -> x == y) t
