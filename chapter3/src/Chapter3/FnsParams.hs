0f9df882e51e6d484e742673d2fd8b68e7b32dea
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2016-08-06 12:05:56 -0700

    Add anonymous function matching multiple patterns with case statement

--- chapter3/src/Chapter3/FnsParams.hs
+++ chapter3/src/Chapter3/FnsParams.hs
@@ -5,3 +5,9 @@ apply3f2 f x = 3 * f(x + 2)
 
 equalTuples :: [(Integer, Integer)] -> [Bool]
 equalTuples t = map (\(x, y) -> x == y) t
+
+sayHello :: [String] -> [String]
+sayHello names = map (\name -> case name of
+                               "Alejandro" -> "Hello, writer"
+                               _           -> "Welcome, " ++ name
+                     ) names
